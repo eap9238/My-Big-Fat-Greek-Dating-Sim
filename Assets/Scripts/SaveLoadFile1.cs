9e0bfdddc902ac5e77904c4964f3d5238ef5310b
Eleanor Post (RIT Student) <eap9238@ad.rit.edu>
2018-09-13 10:49:46 -0400

    Completed the saving system, began adding genders to the sim setup.

---
+++ Assets/Scripts/SaveLoadFile1.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using UnityEngine;
+
+public static class SaveLoadFile1 {
+
+	
+}
